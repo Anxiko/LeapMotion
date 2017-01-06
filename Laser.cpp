4107971c2effc96383ebd7607bd7cacac6599a73
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2017-01-06 18:42:56 +0100

    Fixing git problem (from 335483e)

--- Laser.cpp
+++ Laser.cpp
@@ -71,6 +71,12 @@ namespace DSI
         return tth>=0 && tth<1;
     }
 
+    //Return the energy of this laser
+    EnergyT Laser::get_dmg() const
+    {
+        return lm.damage;
+    }
+
     /*Contact*/
 
     //Check for contact with another shape
@@ -119,6 +125,27 @@ namespace DSI
     void ArrayLaser::add_laser(const LaserModel &ilm, const arrow::Vct &iposition, arrow::Vct ispeed)
     {
         lasers.emplace_back(ilm,iposition,ispeed);
+    }
+
+    /*Hit*/
+
+    //Return damage made to a shape
+    EnergyT ArrayLaser::hit(const arrow::Shp &shp)
+    {
+        EnergyT rv=0;//Total damage
+
+        for (auto it=lasers.begin();it!=lasers.end();)
+        {
+            if (it->hit(shp))//Laser hits the shape
+            {
+                rv+=it->get_dmg();
+                it=lasers.erase(it);
+            }
+            else
+                ++it;
+        }
+
+        return rv;
     }
 
 }
