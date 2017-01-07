c4f9c0fc9a849da8419c749f14dec5db89f7dce0
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2017-01-07 19:37:48 +0100

    Menu implemented

--- main.cpp
+++ main.cpp
@@ -15,12 +15,14 @@
 
 using namespace fdx;
 
+int welcome_screen(const int WINDOW_WIDTH, const int WINDOW_HEIGHT, sf::RenderWindow &window, int const * player_score=nullptr);
+int play_game (const int WINDOW_WIDTH, const int WINDOW_HEIGHT, sf::RenderWindow &window, int &player_points);
+
 int main()
 {
 	//Window size
 	constexpr int WINDOW_WIDTH = 800;
 	constexpr int WINDOW_HEIGHT = 600;
-	const arrow::Rct WINDOW_RCT(arrow::Vct(0,0),arrow::Vct(WINDOW_WIDTH,WINDOW_HEIGHT));
 
 	//Window name
 	const char * const WINDOW_NAME = "LeapMotion";
@@ -28,10 +30,27 @@ int main()
 	//Create the window
 	constexpr int FPS=60;
 
+	//Player score
+	int player_score=0;
+
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_NAME, sf::Style::Close);
 	window.setFramerateLimit(FPS);
 
-	//Font
+	if (welcome_screen(WINDOW_WIDTH,WINDOW_HEIGHT,window))
+        return 1;
+    while(window.isOpen())
+    {
+        if (play_game(WINDOW_WIDTH,WINDOW_HEIGHT,window,player_score)||welcome_screen(WINDOW_WIDTH,WINDOW_HEIGHT,window,&player_score))
+            return 1;
+    }
+}
+
+int play_game (const int WINDOW_WIDTH, const int WINDOW_HEIGHT, sf::RenderWindow &window, int &player_points)
+{
+    //Window size
+    const arrow::Rct WINDOW_RCT(arrow::Vct(0,0),arrow::Vct(WINDOW_WIDTH,WINDOW_HEIGHT));
+
+    //Font
 	sf::Font ft;
 	if (!ft.loadFromFile("media/fonts/ethnocentric rg.ttf"))
     {
@@ -40,26 +59,26 @@ int main()
     }
 
 	//HP_HUD
-	constexpr int HP_HUD_X=WINDOW_WIDTH-110;
-	constexpr int HP_HUD_Y=WINDOW_HEIGHT-50;
+	const int HP_HUD_X=WINDOW_WIDTH-110;
+	const int HP_HUD_Y=WINDOW_HEIGHT-50;
 
 	DSI::Value_HUD hp_hud(ft,HP_HUD_X,HP_HUD_Y,"+ ");
 
 	//XP_HUD
-	constexpr int XP_HUD_X=10;
-	constexpr int XP_HUD_Y=WINDOW_HEIGHT-50;
+	const int XP_HUD_X=10;
+	const int XP_HUD_Y=WINDOW_HEIGHT-50;
 
 	DSI::Value_HUD xp_hud(ft,XP_HUD_X,XP_HUD_Y,"XP: ");
 
 	//KS_HUD
-	constexpr int KS_HUD_X=10;
-	constexpr int KS_HUD_Y=10;
+	const int KS_HUD_X=10;
+	const int KS_HUD_Y=10;
 
 	DSI::Value_HUD ks_hud(ft,KS_HUD_X,KS_HUD_Y,"KS: ");
 
 	//AB_HUD
-	constexpr int AB_HUD_X=WINDOW_WIDTH-185;
-	constexpr int AB_HUD_Y=10;
+	const int AB_HUD_X=WINDOW_WIDTH-185;
+	const int AB_HUD_Y=10;
 
 	DSI::Value_HUD ab_hud(ft,AB_HUD_X,AB_HUD_Y,"Bomb: ");
 
@@ -71,7 +90,7 @@ int main()
         return 1;
     }
 
-    constexpr int WINDOW_BACKGROUND_SPEED=1;
+    const int WINDOW_BACKGROUND_SPEED=1;
     DSI::Background background(background_texture,WINDOW_WIDTH,WINDOW_HEIGHT,WINDOW_BACKGROUND_SPEED);
 
 	//Flash bomb
@@ -85,7 +104,7 @@ int main()
     sf::Sprite bomb_sprite(bomb_texture);
     bomb_sprite.setScale(1.0*WINDOW_WIDTH/bomb_texture.getSize().x,1.0*WINDOW_HEIGHT/bomb_texture.getSize().y);
 
-	constexpr int BOMB_FLASH_TICKS=30;
+	const int BOMB_FLASH_TICKS=30;
 	int flash_ticks=0;//Ticks to flash the screen when a bomb goes off
 
 	//Array Laser
@@ -93,26 +112,26 @@ int main()
 	DSI::ArrayLaser enemy_array;//Lasers from the enemy
 
 	//Player laser model
-	constexpr int PLAYER_LASER_DMG = 25;
-	constexpr int PLAYER_LASER_SZ = 7;
-	constexpr int PLAYER_LASER_TICKS=20;
-	constexpr arrow::Vct::Mod PLAYER_LASER_SPEED = 5;
+	const int PLAYER_LASER_DMG = 25;
+	const int PLAYER_LASER_SZ = 7;
+	const int PLAYER_LASER_TICKS=20;
+	const arrow::Vct::Mod PLAYER_LASER_SPEED = 5;
 	const sf::Color &PLAYER_LASER_COLOR = sf::Color::Magenta;
 
 	const DSI::LaserModel player_laser_model(PLAYER_LASER_DMG,PLAYER_LASER_TICKS,PLAYER_LASER_SZ,PLAYER_LASER_SPEED,PLAYER_LASER_COLOR);
 
 	//Enemy laser model
-	constexpr int ENEMY_LASER_DMG=40;
-	constexpr int ENEMY_LASER_SZ=7;
-	constexpr int ENEMY_LASER_TICKS=20;
-	constexpr arrow::Vct::Mod ENEMY_LASER_SPEED=2;
+	const int ENEMY_LASER_DMG=40;
+	const int ENEMY_LASER_SZ=7;
+	const int ENEMY_LASER_TICKS=20;
+	const arrow::Vct::Mod ENEMY_LASER_SPEED=2;
 	const sf::Color &ENEMY_LASER_COLOR = sf::Color::Red;
 
 	const DSI::LaserModel enemy_laser_model(ENEMY_LASER_DMG,ENEMY_LASER_TICKS,ENEMY_LASER_SZ,ENEMY_LASER_SPEED,ENEMY_LASER_COLOR);
 
 	//Player world
 	std::vector<arrow::Rct> walls;
-	constexpr int WALL_THICKNESS=10;
+	const int WALL_THICKNESS=10;
 
 	walls.push_back(arrow::Rct(arrow::Vct(0,-WALL_THICKNESS),arrow::Vct(WINDOW_WIDTH,WALL_THICKNESS)));//Up
 	walls.push_back(arrow::Rct(arrow::Vct(WINDOW_WIDTH,0),arrow::Vct(WALL_THICKNESS,WINDOW_HEIGHT)));//Right
@@ -141,25 +160,25 @@ int main()
     }
 
     //Player ship
-    constexpr DSI::EnergyT PLAYER_ENER=100;
+    const DSI::EnergyT PLAYER_ENER=100;
     DSI::Ship player_ship(arrow::Vct(WINDOW_WIDTH/2,WINDOW_HEIGHT/2),player_texture,DSI::Ship::ORIENTATION_UP,player_world, player_laser_model,PLAYER_ENER,new DSI::ControllerMouse(WINDOW_WIDTH/2,WINDOW_HEIGHT/2,window));
 
-    int player_points=0;//Points the p`layer has obtained
+    player_points=0;//Points the player has obtained
     int player_kill_streak=0;//Number of ships killed by the player before getting hit a single time
     bool player_ability_ready=false;//Player has the ability ready to use
-    constexpr int PLAYER_KILLSTREAK_ABILITY=2;//Killstreak ready to unlock the ability
-    constexpr int PLAYER_RECHARGE_ENER_PER_KILL=10;//Energy recharged per kill
-    constexpr int PLAYER_POINTS_PER_KILL=10;//Points gained per kill
+    const int PLAYER_KILLSTREAK_ABILITY=2;//Killstreak ready to unlock the ability
+    const int PLAYER_RECHARGE_ENER_PER_KILL=10;//Energy recharged per kill
+    const int PLAYER_POINTS_PER_KILL=10;//Points gained per kill
 
     //Enemy ships
     //DSI::Ship enemy_ship(arrow::Vct(WINDOW_WIDTH/2,40),player_texture,DSI::Ship::ORIENTATION_DOWN,enemy_world,player_laser_model,PLAYER_ENER);
-    constexpr int ENEMY_SHIPS=10;
-    constexpr int ENEMY_ENER=25;
+    const int ENEMY_SHIPS=10;
+    const int ENEMY_ENER=25;
 
     DSI::EnemyManager enemy_ships(ENEMY_SHIPS,WINDOW_RCT,enemy_texture,enemy_laser_model,ENEMY_ENER,enemy_world);
 
 	//Main loop
-	while (window.isOpen())
+	while (window.isOpen()&&player_ship.is_alive())
 	{
 		//Process events
 		sf::Event e;
@@ -222,7 +241,6 @@ int main()
         if (flash_ticks)
 		{
 		    sf::Uint8 alpha = (255.0*flash_ticks)/BOMB_FLASH_TICKS;
-		    std::cout<<"A: "<<+alpha;
             bomb_sprite.setColor(sf::Color(255,255,255,alpha));
             window.draw(bomb_sprite);
             --flash_ticks;
@@ -240,4 +258,86 @@ int main()
 	}
 
 	return 0;
+}
+
+int welcome_screen(const int WINDOW_WIDTH, const int WINDOW_HEIGHT, sf::RenderWindow &window, int const * player_score)
+{
+    //Text to print
+    //Font
+	sf::Font ft;
+	if (!ft.loadFromFile("media/fonts/ethnocentric rg.ttf"))
+    {
+        std::cerr<<"Error loading the font"<<std::endl;
+        return 1;
+    }
+
+    sf::FloatRect textRect;
+
+    //Title text
+    const int TITLE_TEXT_Y=WINDOW_HEIGHT/2-100;
+
+    sf::Text title_text(std::string("Hand_Ship"),ft);
+
+    textRect = title_text.getLocalBounds();
+    title_text.setOrigin(textRect.left + textRect.width/2.0f, textRect.top  + textRect.height/2.0f);
+
+    title_text.setPosition(WINDOW_WIDTH/2,TITLE_TEXT_Y);
+
+
+    //Enter text
+    const int ENTER_TEXT_Y=WINDOW_HEIGHT/2;
+
+    sf::Text enter_text(std::string("Press Enter to play"),ft);
+
+    textRect = enter_text.getLocalBounds();
+    enter_text.setOrigin(textRect.left + textRect.width/2.0f, textRect.top  + textRect.height/2.0f);
+
+    enter_text.setPosition(WINDOW_WIDTH/2,ENTER_TEXT_Y);
+
+    //Score text
+    const int SCORE_TEXT_Y=WINDOW_HEIGHT-50;
+
+    std::stringstream ss;
+    ss<<"Previous score: "<<(player_score?*player_score:0);
+
+    sf::Text score_text(ss.str(),ft);
+    textRect = score_text.getLocalBounds();
+    score_text.setOrigin(textRect.left + textRect.width/2.0f, textRect.top  + textRect.height/2.0f);
+
+    score_text.setPosition(WINDOW_WIDTH/2,SCORE_TEXT_Y);
+
+    while (window.isOpen())
+    {
+        sf::Event e;
+        while(window.pollEvent(e))
+        {
+            switch (e.type)
+            {
+                case sf::Event::Closed:
+                {
+                    window.close();
+                    break;
+                }
+
+                case sf::Event::KeyPressed:
+                {
+                    if (e.key.code==sf::Keyboard::Return)
+                        return 0;
+                }
+
+                default:
+                    break;
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        window.draw(title_text);
+        window.draw(enter_text);
+        if (player_score)
+            window.draw(score_text);
+        window.display();
+
+    }
+
+    return 0;
 }
